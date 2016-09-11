9b1f0fb0ce8e024c4744e3f74654eddb0834d26b
Dwiyan Rahmanianto <rahmanianto@gmail.com>
2016-09-11 19:26:30 +0700

    moving code to js file

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,3 +12,4 @@ $(function() {
       }
     });
   });
+(function(d,s,id){var z=d.createElement(s);z.type="text/javascript";z.id=id;z.async=true;z.src="//static.zotabox.com/d/e/de11a162b525affaf964709743bf41c1/widgets.js";var sz=d.getElementsByTagName(s)[0];sz.parentNode.insertBefore(z,sz)}(document,"script","zb-embed-code"));
